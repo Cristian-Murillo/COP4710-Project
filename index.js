51c1979cc21a15bd019636e64a25046e27a917c9
Cristian Murillo <crismu97@gmail.com>
2022-03-06 11:33:12 -0500

    Deleted Handled Disconnect Function
    No need to end and reconnect connections when we already
    connected to DB from createConnection() in db.js

--- index.js
+++ index.js
@@ -2,33 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const db = require("./config/db");
-const Connection = require("mysql/lib/Connection");
 
 const PORT = process.env.PORT || 5000;
-
-// function to handle disconnection of db server that may have went down or restarted
-function handleDisconnect() {
-  db.connect(function (err) {
-    if (err) {
-      console.log("Error when connecting to DB: " + err);
-      setTimeout(handleDisconnect(), 2000);
-    } else {
-      console.log("MySQL Database Connected");
-    }
-  });
-
-  db.on("error", function (err) {
-    console.log("db.error", err);
-    if (err.code === "PROTOCOL_CONNECTION_LOST") {
-      handleDisconnect();
-    } else {
-      throw err;
-    }
-  });
-}
-
-handleDisconnect();
 
 app.set("port", PORT);
 
@@ -36,7 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 // routes
-
 app.use("/api/users", require("./routes/users"));
 
 app.listen(PORT, () => {
