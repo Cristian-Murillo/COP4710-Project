68727e21b2946e3021355533d6faaab01b12495e
Cristian Murillo <crismu97@gmail.com>
2022-03-06 22:17:33 -0500

    Fix Fatal Error MySQL
    Fixed PROTOCOL_CONNECTION_ERROR creating and ending connection
    for each request due to connection always timing out

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
 const express = require("express");
+const mysql = require("mysql");
 const router = express.Router();
-const db = require("../config/db");
+require("dotenv").config();
+
+var db;
+
+// Function to start connection to DB before query
+function connectDB() {
+  db = mysql.createConnection({
+    host: process.env.SQLHOST,
+    user: process.env.SQLUSER,
+    port: process.env.SQLPORT,
+    password: process.env.SQLPASSWORD,
+    database: process.env.SQLDATABASE,
+  });
+
+  db.connect(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("MySQL Database Connected");
+  });
+}
+
+// Function to end connection to prevent fatal error
+// always end connection after creating one
+function disconnectDB() {
+  db.end(function (err) {
+    if (err) throw err;
+    console.log("DISCONNECTED FROM DB");
+  });
+}
 
 // get all users
 router.get("/", async (req, res) => {
+  connectDB();
   db.query("SELECT * FROM user", (error, result) => {
     if (error) {
       console.log(error);
@@ -11,6 +42,7 @@ router.get("/", async (req, res) => {
       res.send(result);
     }
   });
+  disconnectDB();
 });
 
 module.exports = router;
