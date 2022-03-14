9ac1a53c15fb961f906ef4c8df982a034844a591
Cristian Murillo <crismu97@gmail.com>
2022-03-14 15:02:13 -0400

    defined path to fix heroku

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 app.set("port", PORT);
